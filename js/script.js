73b20b2036753cde258caf68df81bd191c3d0c67
Viktoria <bazykviktoria@gmail.com>
2021-07-21 11:04:05 +0200

    turned functions into methods of object personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,89 +1,112 @@
 'use strict';
 
 
-let numberOfMovies; 
-// + converts a string to a number, in this case we work with numbers 
-
 /* isNan this is a method that takes an argument inside itself, 
    check it and if we don't have a number inside, than it returns 
    the truth that is not a number
- */
-function start() {
-    numberOfMovies = +prompt("How many movies have you watched?", ' ');
+*/
 
-    while (numberOfMovies == '' || numberOfMovies == null || isNaN(numberOfMovies)) { 
-        numberOfMovies = +prompt("How many movies have you watched?", ' ');
-    }
-}
-// start();
+
 
 const personalMovieDB = {
-    count: numberOfMovies,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt("How many movies have you watched?", ' ');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) { 
+            personalMovieDB.count = +prompt("How many movies have you watched?", ' ');
+        }
+    },
+    rememberMyMovies: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('One of the last viewd movies?', ' '),
+                  b = prompt('What rating will You give?', ' ');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectedPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log('Quit a few movies have been viewd');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('You are a classic viewer');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('You are a movie fan');
+        } else {
+            console.log('Error');
+        }
+    },
+    showMyDB: function() {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Your favorite genre is under the number ${i}`);
+            
+            if (genre === '' || genre == null) {
+                console.log('You didn\'t specify the genre');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre; // -1 because in the let we don't start from zero
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Favorite genre ${i + 1} is ${item}`);
+        }); 
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
 };
 
-/* const a = prompt('One of the last viewd movies?', ' '), // ниже приведен автоматизированный код
-      b = prompt('What rating will You give?', ' '),
-      c = prompt('One of the last viewd movies?', ' '),
-      d = prompt('What rating will You give?', ' ');
+personalMovieDB.start();
+personalMovieDB.rememberMyMovies();
+personalMovieDB.detectedPersonalLevel();
+personalMovieDB.showMyDB();
+personalMovieDB.writeYourGenres();
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; */ 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// We wrapped the loop in the function to call it as needed
-function rememberMyMovies() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('One of the last viewd movies?', ' '),
-              b = prompt('What rating will You give?', ' ');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
 
-// rememberMyMovies ();
 
-function detectedPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Quit a few movies have been viewd');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('You are a classic viewer');
-    } else if (personalMovieDB.count >= 30) {
-        console.log('You are a movie fan');
-    } else {
-        console.log('Error');
-    }
-}
-
-// detectedPersonalLevel();
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* function showMyDB(hidden) { // teacher's example
     if (!hidden) { // the value false from DB is passed here and ! chanched it to true
         console.log(personalMovieDB);
     }
 }
-
 showMyDB(personalMovieDB.privat); */ 
 
-function showMyDB() {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    }
-}
+ 
 
-showMyDB();
 
-function writeYourGenres() {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+
+
+// code optimization, deleted const 
+/* function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Your favorite genre is under the number ${i}`, ' ');
-        personalMovieDB.genres[i - 1] = genre; // -1 because in the let we don't start from zero
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite genre is under the number ${i}`, ' '); 
     }
 }
-writeYourGenres();
+writeYourGenres(); */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
